a12ba038cec041b01f3e5f5b1ccd6a8b1cc047a3
q0x6y6666 <followify@proton.me>
2024-10-13 19:38:38 -0600

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["💻 Using VALKOR", "🎥 Watching VALKOR"];
+const statusMessages = ["Made with love", "Watching 0x6"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
