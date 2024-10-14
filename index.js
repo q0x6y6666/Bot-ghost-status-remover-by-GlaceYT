daa36fbb75333a052139a3521c3cb36dec92bf34
q0x6y6666 <followify@proton.me>
2024-10-14 14:25:45 -0600

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Made with love", "Watching 0x6"];
+const statusMessages = ["", ""];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
