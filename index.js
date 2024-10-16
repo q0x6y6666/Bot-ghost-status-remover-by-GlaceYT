823a4cd786398223b304f1b71a8a019e3761a133
q0x6y6666 <followify@proton.me>
2024-10-15 18:59:32 -0600

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Watching /moneyz", "Watching /moneyz"];
+const statusMessages = ["👑 PotsVerse On Top!", "👑 PotsVerse On Top!"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
