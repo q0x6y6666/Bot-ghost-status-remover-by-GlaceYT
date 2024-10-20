049fd76aa1a5c9d653eb67e01ecabce739af6bae
q0x6y6666 <followify@proton.me>
2024-10-20 14:52:39 -0600

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Watching keys", "Watching users"];
+const statusMessages = ["Watching Tickets", "Watching Xeno"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
